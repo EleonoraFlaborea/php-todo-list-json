048f5f584641570d0570796cb993292b13febe03
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-22 15:29:30 +0100

    add structure

--- index.php
+++ index.php
@@ -7,9 +7,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Todo list</title>
+
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!--My stylesheet-->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--Vue 3-->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!--JS-->
+    <script defer src="js/script.js"></script>
+
+    <!--AXIOS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.7/axios.min.js" integrity="sha512-NQfB/bDaB8kaSXF8E77JjhHG5PM6XVRxvHzkZiwl3ddWCEPBa23T76MuWSwAJdMGJnmQqM0VeY9kFszsrBEFrQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
-<body>
-    
+<body  class="bg-primary-subtle">
+
+<header>
+   <div class="container text-white py-5 text-center">
+       <h1>TODO LIST</h1>
+   </div>
+
+</header>
+
+<main>
+    <div class="d-flex justify-content-center">
+        <div class="bg-warning-subtle todo-container rounded py-2 px-2">
+            <p>ciao</p>
+        </div>
+    </div>
+
+</main>
 </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log('Vue on', Vue)
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+
+
+    }
+})
+
+app.mount('#root');
