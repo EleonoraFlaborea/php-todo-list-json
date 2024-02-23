65e650095bfcae536dc8363a494b3e5b0b9375fb
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-23 10:26:16 +0100

    fixed add task structure

--- api/index.php
+++ api/index.php
@@ -1,10 +1,41 @@
 <?php 
+header('Content-Type: application/json');
 
-$tasks = json_decode(file_get_contents('dati.json'));
+header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+$source_path = __DIR__ . './../api/dati.json';
+$json_data = file_get_contents($source_path);
+$tasks = $json_data;
+
+//$tasks = json_decode(file_get_contents('dati.json')); 
 
 
-        header('Content-Type: application/json');
-        echo json_encode($tasks, true);
+$task_text = $_POST['task'] ?? '';
+
+//se ho un nuovo task da aggiungere
+if($task_text){
+    //converto il task in array php
+   $tasks = json_decode($tasks, true);
+
+   $new_task = [
+    'done' => false,
+    'text' => $task_text,
+    'id' => uniqid(),
+   ];
+
+   //aggiungo il task 
+   $tasks[] = $new_task;
+
+   //converto il task in json
+   $tasks = json_encode($tasks);
+
+   //array aggiornato
+   file_put_contents($source_path, $tasks);
+};
+
+
+echo $tasks;
 
 ?>
 
--- js/script.js
+++ js/script.js
@@ -8,6 +8,19 @@ const app = createApp({
     data: () => ({
         tasks: []
     }),
+    methods: {
+        addTask() {
+            const data = { task: this.newTask }
+            const config = {
+                params: { new: 'PHP' },
+                headers: { 'Content-Type': 'multipart/form-data' }
+            }
+            axios.post(endpoint, data, config).then(res => {
+                this.tasks = res.data;
+                this.newTask = '';
+            })
+        }
+    },
     created() {
         axios.get(endpoint).then(res => {
             this.tasks = res.data;
